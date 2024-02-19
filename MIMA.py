9530778445095ee70205adf06f2b3e01c7ececd3
ruilin <110581221+69936876@users.noreply.github.com>
2024-02-19 15:07:49 +0800

    Update MIMA.py

--- MIMA.py
+++ MIMA.py
@@ -1,10 +1,19 @@
-key="888888"
+key="12345"
 k=input("请输入密码")
 if k==key:
        print("欢迎你")
        print("密码正确，程序已正常关机")
+       import time
+
+       localtime = time.asctime(time.localtime(time.time()))
+       print("本地时间 :", localtime)
+       print("（程序已结束运行）")
 else:
     print("密码错误")
     print("程序已强制停止")
-    
-    
+
+    import time
+    localtime = time.asctime(time.localtime(time.time()))
+    print("本地时间 :", localtime)
+    print("（程序已结束运行）")
+
