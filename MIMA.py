5689057f820e530990c2ad415a0bd4069f0e3cb8
ruilin <110581221+69936876@users.noreply.github.com>
2024-03-05 18:05:43 +0800

    Update MIMA.py

--- MIMA.py
+++ MIMA.py
@@ -1,8 +1,8 @@
 key="12345"
 k=input("请输入密码")
 if k==key:
-       print("欢迎你")
        print("密码正确")
+       print("欢迎你")
        import time
 
        localtime = time.asctime(time.localtime(time.time()))
